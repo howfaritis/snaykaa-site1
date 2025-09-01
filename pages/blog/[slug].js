bb8f18caf7fb2d7c6ed0ce04111e3c8b161a7f84
howfaritis <howfaritis@gmail.com>
2025-09-01 11:16:55 -0500

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,8 +1,24 @@
-
 import { useRouter } from 'next/router'
 import blog from '../../data/blog.json'
+import { marked } from 'marked'
+
 export default function Post(){
   const { slug } = useRouter().query
   const p = blog.posts.find(x=>x.slug===slug) || blog.posts[0]
-  return (<div className="container"><a href="/blog">← Back</a><h1>{p.title}</h1><p>{p.body}</p></div>)
+
+  // Convert Markdown (###, **bold**, - lists, etc.) to HTML
+  const html = marked.parse(p.body || '')
+
+  return (
+    <div className="container">
+      <a href="/blog">← Back</a>
+      <h1 style={{ marginTop: 12, marginBottom: 12 }}>{p.title}</h1>
+      <div
+        className="prose"
+        style={{ lineHeight: 1.7 }}
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    </div>
+  )
 }
+
