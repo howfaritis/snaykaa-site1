9a0958d5017500daae008a254c16e707b10bedc1
howfaritis <howfaritis@gmail.com>
2025-09-02 12:40:04 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 // pages/_app.js
 import "../styles/globals.css";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      <main className="page-main">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
     </>
   );
 }
