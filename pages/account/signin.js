8c8c6adb58a5505a7929e890cb9dbf0a17786e7e
howfaritis <howfaritis@gmail.com>
2025-09-02 14:54:19 -0500

    Create signin.js

---
+++ pages/account/signin.js
@@ -0,0 +1,13 @@
+import SeoHead from "../../components/SeoHead";
+
+export default function SignIn() {
+  return (
+    <>
+      <SeoHead title="Sign In" />
+      <div className="container prose">
+        <h1>Sign In</h1>
+        <p>Coming soon. For now, email <a href="mailto:snaykaabrand@gmail.com">snaykaabrand@gmail.com</a> and we’ll help you with orders.</p>
+      </div>
+    </>
+  );
+}
