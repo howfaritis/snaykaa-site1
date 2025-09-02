8bc9848095cf667280135034e0cf2f9625fbdbfe
howfaritis <howfaritis@gmail.com>
2025-09-02 14:39:54 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
-// pages/_app.js
 import "../styles/globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
-      <main className="page-main">
+      <main className="page-wrap">
         <Component {...pageProps} />
       </main>
       <Footer />
