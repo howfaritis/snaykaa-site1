f38d68ce07916dc1e4aa513d7f07bf75c7f5500a
howfaritis <howfaritis@gmail.com>
2025-09-02 14:54:53 -0500

    Create signup.js

---
+++ pages/account/signup.js
@@ -0,0 +1,13 @@
+import SeoHead from "../../components/SeoHead";
+
+export default function SignUp() {
+  return (
+    <>
+      <SeoHead title="Create Account" />
+      <div className="container prose">
+        <h1>Create Account</h1>
+        <p>Account system is coming soon. Want early access? <a href="mailto:snaykaabrand@gmail.com?subject=Account%20Request">Email us</a>.</p>
+      </div>
+    </>
+  );
+}
