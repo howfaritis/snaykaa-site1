de365b5595d2d51348fbfab7d20905e1ea3164b5
howfaritis <howfaritis@gmail.com>
2025-09-02 14:42:47 -0500

    Update privacy.js

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,1 +1,13 @@
-export default function Privacy(){return (<div className="container"><h1>Privacy Policy</h1><p>We respect your privacy. This site uses basic analytics.</p></div>)}
+import SeoHead from "../components/SeoHead";
+
+export default function Privacy() {
+  return (
+    <>
+      <SeoHead title="Privacy Policy" />
+      <div className="container prose">
+        <h1>Privacy Policy</h1>
+        <p>We respect your privacy. This website collects only the minimum information required to run the site and improve your experience. We do not sell personal data.</p>
+      </div>
+    </>
+  );
+}
