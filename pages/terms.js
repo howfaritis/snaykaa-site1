455efbb8dfd42f36e37f994ad73803ffec2387e1
howfaritis <howfaritis@gmail.com>
2025-09-02 14:43:41 -0500

    Create terms.js

---
+++ pages/terms.js
@@ -0,0 +1,13 @@
+import SeoHead from "../components/SeoHead";
+
+export default function Terms() {
+  return (
+    <>
+      <SeoHead title="Terms & Conditions" />
+      <div className="container prose">
+        <h1>Terms &amp; Conditions</h1>
+        <p>By using snaykaa.com you agree to our terms of use, which include acceptable use of the site, intellectual property, and limitation of liability.</p>
+      </div>
+    </>
+  );
+}
