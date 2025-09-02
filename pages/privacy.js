6088f64fd4ab9e6d01a55af3d1ea9848ebcb6acc
howfaritis <howfaritis@gmail.com>
2025-09-02 15:57:06 -0500

    Update privacy.js

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,12 +1,12 @@
 import SeoHead from "../components/SeoHead";
-
-export default function Privacy() {
+export default function Privacy(){
   return (
     <>
       <SeoHead title="Privacy Policy" />
       <div className="container prose">
         <h1>Privacy Policy</h1>
-        <p>We respect your privacy. This website collects only the minimum information required to run the site and improve your experience. We do not sell personal data.</p>
+        <p>We respect your privacy. This website collects only the minimum information required to operate and improve your experience. We do not sell personal data.</p>
+        <p>Questions? <a href="mailto:snaykaabrand@gmail.com">snaykaabrand@gmail.com</a></p>
       </div>
     </>
   );
